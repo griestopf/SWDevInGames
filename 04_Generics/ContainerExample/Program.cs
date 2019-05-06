0fc6c6ce62d092b5cb88e10493751e250a7a857f
Christoph <Mller>
2019-05-06 14:37:02 +0200

    ContainerExample

---
+++ 04_Generics/ContainerExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ContainerExample
+{
+
+    public class MyContainer
+    {
+        private int[] _theObjects;
+        private int _n;
+
+        public MyContainer()
+        {
+            _theObjects = new int[4];
+            _n = 0;
+        }
+
+        public void Add(int i)
+        {
+            _theObjects[_n++] = i;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyContainer myContainer = new MyContainer();
+
+            myContainer.Add(2);
+            myContainer.Add(4711);
+            myContainer.Add(123);
+            myContainer.Add(456);
+
+        }
+    }
+}
